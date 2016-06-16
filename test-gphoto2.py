3a3f9151faf22e657db263497fa4fab0808a9739
iosonosempreio <tommaso.elli@gmail.com>
2016-06-16 10:26:05 +0200

    capture, save, process and open

--- test-gphoto2.py
+++ test-gphoto2.py
@@ -3,16 +3,25 @@
 #!/usr/bin/python
 
 import subprocess
+import os
 
 from PIL import Image
 
-# gpout = subprocess.check_output("gphoto2 --get-file 2690", stderr=subprocess.STDOUT, shell=True);
+gpout = subprocess.check_output("gphoto2 --capture-image-and-download", stderr=subprocess.STDOUT, shell=True);
 
-# print(gpout)
+gpout = gpout.split()
+
+print(gpout[12])
+
+c = 1;
+
+newName = gpout[12] + '_c' + '.jpg'
+
+os.rename(gpout[12],newName)
 
 # imageFile = gpout.split()[3]
 
-imageFile = "test.jpg"
+imageFile = newName
 
 # open an image file (.bmp,.jpg,.png,.gif) you have in the working folder
 im1 = Image.open(imageFile)
@@ -21,7 +30,7 @@ print(im1.size[0])
 print(im1.size[1])
 
 # adjust width and height to your needs
-width = 800
+width = 200
 height = (width * im1.size[1])/im1.size[0]
 
 
@@ -36,5 +45,21 @@ print(imageFile + " resized")
 
 # output = subprocess.check_output("python pixelsort/pixelsort.py " + imageFile + " -a -45 -i waves -c 30", stderr=subprocess.STDOUT, shell=True)
 
-output = subprocess.check_output("python pixelsort/pixelsort.py " + imageFile + " -i threshold -t 0.2 -u 0.6", stderr=subprocess.STDOUT, shell=True)
+#pixelsortSubprocess = "python pixelsort/pixelsort.py " + imageFile + " -i threshold -t 0.2 -u 0.6"
 
+pixelsortSubprocess = "python pixelsort/pixelsort.py " + imageFile + " -a -45 -i waves -c 30"
+
+output = subprocess.check_output(pixelsortSubprocess, stderr=subprocess.STDOUT, shell=True)
+
+output = output.split()
+
+output = output[-1]
+
+output = output.replace(output[:1],'')
+
+output = output.replace(')','')
+
+print output
+
+img = Image.open(output)
+img.show()
